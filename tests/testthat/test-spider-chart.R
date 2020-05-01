9677887300be86e0853a33e0b17ad1c4d08f0c9f
DeclanMolony <declanmolony@yahoo.com>
2020-05-01 13:38:24 -0700

    Update test-spider-chart.R

--- tests/testthat/test-spider-chart.R
+++ tests/testthat/test-spider-chart.R
@@ -1,4 +1,10 @@
+test_that("city_link() provides the Teleport API link to a specific city",{
+  correct_result <- "https://api.teleport.org/api/urban_areas/slug:dublin/scores/"
 
+  my_result <- city_link("Dublin")
+
+  expect_equal(my_result, correct_result)
+})
 
 
 
