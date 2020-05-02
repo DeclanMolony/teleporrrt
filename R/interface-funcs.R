60712dabb1c820bad14cf82c7bdbfce348a63612
Notchanator <55802661+Notchanator@users.noreply.github.com>
2020-05-02 12:24:39 -0700

    changed the @return

--- R/interface-funcs.R
+++ R/interface-funcs.R
@@ -6,7 +6,7 @@
 #' @param lat the latitude position to search with
 #' @param lon the longitude position to search with
 #'
-#' @return A data frame with html link, closest city name, and distance(km)
+#' @return A data frame with closest city name and distance(km)
 #'
 #' @importFrom httr GET
 #' @importFrom jsonlite fromJSON
@@ -21,7 +21,7 @@ nearest_city <- function(lat, lon){
 
   content <- fromJSON(rawToChar(response$content))$`_embedded`$`location:nearest-cities`
 
-  distance <- as.numeric(content$distance_km)
+  distance_km <- as.numeric(content$distance_km)
 
   nearest_city <- content$`_links`$`location:nearest-city`$name
 
@@ -29,7 +29,7 @@ nearest_city <- function(lat, lon){
     return("Invalid Lat/Lon coordinate")
   }
 
-  nearest_df <- cbind(nearest_city, distance_km = distance)
+  nearest_df <- cbind(nearest_city, distance_km)
 
   return(nearest_df)
 
